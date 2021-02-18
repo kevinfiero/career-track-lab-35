ce7c5d91b75ee1527f6658ea157eb46c92d59885
kevinfiero <kjf46@cornell.edu>
2021-02-17 23:20:26 -0800

    color styling

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PostForm from '../../forms/PostForm';
 import { PostProvider } from '../../state/PostProvider';
 import PostList from '../post/PostList';
+import './App.css';
 
 export default function App() {
   return (
